94febd1147f8c60e9e4e8060280113da3fbd47c5
ethanEverel123 <C22351646@mytudublin.ie>
2025-12-13 21:23:17 +0000

    grabbing works again

--- src/godot-xr-template/deleteObjects.gd
+++ src/godot-xr-template/deleteObjects.gd
@@ -1,7 +1,14 @@
-extends Node3D
+@tool
+extends XRToolsPickable
 
-@export var lifetime: float = 5.0
+@export var lifetime := 5.0
 
 func _ready():
+	if Engine.is_editor_hint():
+		return
+
+	await get_tree().process_frame
 	await get_tree().create_timer(lifetime).timeout
-	queue_free()
+
+	if is_inside_tree():
+		queue_free()
--- src/godot-xr-template/trash.gd
+++ src/godot-xr-template/trash.gd
@@ -3,8 +3,9 @@ extends Node3D
 @export var trash: PackedScene
 @export var fish: PackedScene
 @export var spawn_interval: float = 1.0
-@export var SpawnPoint: Marker3D
-var rng = RandomNumberGenerator.new()
+@export var spawn_point: Marker3D
+
+var rng := RandomNumberGenerator.new()
 
 func _ready():
 	var timer := Timer.new()
@@ -13,18 +14,15 @@ func _ready():
 	timer.timeout.connect(_on_spawn_timeout)
 	add_child(timer)
 
-
 func _on_spawn_timeout():
-	var my_random_number = rng.randf_range(0.0, 10.0)
-	if my_random_number > 3:
-		var obj = trash.instantiate()
-		obj.global_transform = SpawnPoint.global_transform
-		get_tree().current_scene.add_child(obj)
+	var obj: Node3D
+
+	if rng.randf() > 0.3:
+		obj = trash.instantiate()
 		obj.add_to_group("trash")
 	else:
-		var obj = fish.instantiate()
-		obj.global_transform = SpawnPoint.global_transform
-		get_tree().current_scene.add_child(obj)
+		obj = fish.instantiate()
 		obj.add_to_group("fish")
 
-		
+	obj.global_transform = spawn_point.global_transform
+	get_tree().current_scene.add_child(obj)
