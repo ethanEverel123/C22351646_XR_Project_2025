d60bf2d0e36402162afa39a49170834c78302851
ethanEverel123 <C22351646@mytudublin.ie>
2025-12-15 00:25:22 +0000

    added an explosion and sound effect for when the user puts trash in the bin

--- src/godot-xr-template/can.gd
+++ src/godot-xr-template/can.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var explosion_scene: PackedScene
+
 func _ready():
 	$DeleteArea.body_entered.connect(_on_body_entered)
 
@@ -7,4 +9,9 @@ func _on_body_entered(body):
 	
 	#delete trash if it enters the can
 	if body.is_in_group("trash"):
+		var explosion:GPUParticles3D = explosion_scene.instantiate()
+		get_parent().add_child(explosion)	
+		explosion.global_position = global_position
+		explosion.emitting = true
+		
 		body.queue_free()
